fad487476d0193776d5bdb8f50f9c3c1b3c8a65e
Bakhtiyor Bakhriddinov <dataeng27@gmail.com>
2025-02-18 19:37:28 +0500

    Set filtering only active media in media/device endpoint

--- app/routes/v1/life_span.py
+++ app/routes/v1/life_span.py
@@ -1,4 +1,4 @@
-from contextlib import asynccontextmanager
+from contextlib import asynccontextmanager, contextmanager
 
 import pytz
 from sqlalchemy.orm import Session
@@ -13,6 +13,7 @@ from app.crud.pages_and_permissions import (
 )
 from app.crud.roles import create_role, create_accesses, get_role_by_name
 from app.crud.accounts import create_account, get_account_by_password
+from app.db.session import SessionLocal
 from app.routes.depth import get_db
 from app.utils.permissions import pages_and_permissions
 
@@ -22,17 +23,25 @@ timezonetash = pytz.timezone('Asia/Tashkent')
 #create new permission
 @asynccontextmanager
 async def create_permissions_lifespan():
-    db: Session = next(get_db())
-    for key, value in pages_and_permissions.items():
-        permission_page = get_permission_page(db=db, name=key)
-        if permission_page:
-            permission_page_id = permission_page.id
-        else:
-            permission_page_id = create_permission_page(db=db, name=key).id
-        for name, link in value.items():
-            permission = get_permission(db=db, link=link, permission_page_id=permission_page_id)
-            if not permission:
-                create_permission(db=db, name=name, link=link, permission_page_id=permission_page_id)
+    @contextmanager
+    def get_db():
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    with get_db() as db:
+        for key, value in pages_and_permissions.items():
+            permission_page = get_permission_page(db=db, name=key)
+            if permission_page:
+                permission_page_id = permission_page.id
+            else:
+                permission_page_id = create_permission_page(db=db, name=key).id
+            for name, link in value.items():
+                permission = get_permission(db=db, link=link, permission_page_id=permission_page_id)
+                if not permission:
+                    create_permission(db=db, name=name, link=link, permission_page_id=permission_page_id)
 
     yield  #--------------  HERE YOU CAN WRITE LOG ON CLOSING AFTER YIELD ------------
 
@@ -40,15 +49,22 @@ async def create_permissions_lifespan():
 #---------------------- CREATE ROLE AND USERS FOR DEFAULT ADMIN USER --------------------------
 @asynccontextmanager
 async def create_role_lifespan():
-    db: Session = next(get_db())
-    role = get_role_by_name(db=db, name=settings.admin_role)
-    if not role:
-        role = create_role(db=db, name=settings.admin_role, description='Superuser')
+    @contextmanager
+    def get_db():
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
 
-    account = get_account_by_password(db=db, password=settings.admin_password)
-    print("ACCOUNT: ", account)
-    if not account:
-        create_account(db=db, password=settings.admin_password, role_id=role.id)
+    with get_db() as db:
+        role = get_role_by_name(db=db, name=settings.admin_role)
+        if not role:
+            role = create_role(db=db, name=settings.admin_role, description='Superuser')
+
+        account = get_account_by_password(db=db, password=settings.admin_password)
+        if not account:
+            create_account(db=db, password=settings.admin_password, role_id=role.id)
 
     role_permissions = []
     for i in role.accesses:
